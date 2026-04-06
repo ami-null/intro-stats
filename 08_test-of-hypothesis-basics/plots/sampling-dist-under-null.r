be9a9043f639b54c7ce628540559be63994201e2
Md. Aminul Islam Shazid <70378126+ami-null@users.noreply.github.com>
2026-04-06 20:25:04 +0600

    create example plots

---
+++ 08_test-of-hypothesis-basics/plots/sampling-dist-under-null.r
@@ -0,0 +1,183 @@
+# Parameters
+x <- seq(-4, 4, length = 1000)
+y <- dnorm(x)
+
+# Plot
+
+pdf("sampling-dist-under-null.pdf", width = 6.5, height = 3.5)
+plot(x, y, type = "l", lwd = 2,
+     main = expression("Sampling Distribution under " * H[0]),
+     xlab = "Test Statistic (Z)", ylab = "Density")
+
+abline(v = 0, lty = 2)
+text(-0.1, max(y)*0.85, expression(mu[0]), pos = 4)
+dev.off()
+
+
+# ----
+
+# Define an arbitrary "unlikely" cutoff (before formal alpha)
+cutoff <- 2
+
+pdf("unlikely-region-under-null.pdf")
+plot(x, y, type = "l", lwd = 2,
+     main = "Unlikely Values under H0",
+     xlab = "Test Statistic (Z)", ylab = "Density")
+
+# Shade right tail
+xx1 <- seq(-4, -z_crit, length = 500)
+yy1 <- dnorm(xx1)
+polygon(c(xx1, rev(xx1)), c(rep(0, length(xx1)), rev(yy1)),
+        density = 20, angle = 135)
+
+# Right tail
+xx2 <- seq(z_crit, 4, length = 500)
+yy2 <- dnorm(xx2)
+polygon(c(xx2, rev(xx2)), c(rep(0, length(xx2)), rev(yy2)),
+        density = 20, angle = 45)
+
+abline(v = cutoff, lty = 2)
+text(cutoff, dnorm(cutoff), "Unlikely", pos = 4)
+
+abline(v = -cutoff, lty = 2)
+text(-cutoff, dnorm(-cutoff), "Unlikely", pos = 2)
+
+text(0, max(y)*0.75, expression(H[0]~"True"))
+dev.off()
+
+
+# ---- alpha
+pdf("two-tail-rejection-region.pdf")
+alpha <- 0.05
+z_crit <- qnorm(1 - alpha/2)
+
+plot(x, y, type = "l", lwd = 2,
+     main = "Rejection Regions (Two-tailed Test)",
+     xlab = "Test Statistic (Z)", ylab = "Density")
+
+# Left tail
+xx1 <- seq(-4, -z_crit, length = 500)
+yy1 <- dnorm(xx1)
+polygon(c(xx1, rev(xx1)), c(rep(0, length(xx1)), rev(yy1)),
+        density = 20, angle = 135)
+
+# Right tail
+xx2 <- seq(z_crit, 4, length = 500)
+yy2 <- dnorm(xx2)
+polygon(c(xx2, rev(xx2)), c(rep(0, length(xx2)), rev(yy2)),
+        density = 20, angle = 45)
+
+# Critical lines
+abline(v = c(-z_crit, z_crit), lty = 2)
+
+# Labels
+# text(-z_crit, dnorm(-z_crit), expression(-z[alpha/2]), pos = 4)
+# text(z_crit, dnorm(z_crit), expression(z[alpha/2]), pos = 2)
+text(-z_crit, 0, expression(-z[alpha/2]), pos = 4)
+text(z_crit, 0, expression(z[alpha/2]), pos = 2)
+
+text(-2.8, 0.05, expression(alpha/2))
+text(2.8, 0.05, expression(alpha/2))
+text(0, 0.3, expression(1 - alpha))
+dev.off()
+
+
+# ---- right tail rejection region
+
+alpha <- 0.05
+z_crit <- qnorm(1 - alpha)
+
+pdf("right-tailed-rejection-region.pdf")
+plot(x, y, type = "l", lwd = 2,
+     main = "Rejection Region (Right-tailed Test)",
+     xlab = "Test Statistic (Z)", ylab = "Density")
+
+# Shade rejection region
+xx <- seq(z_crit, 4, length = 500)
+yy <- dnorm(xx)
+polygon(c(xx, rev(xx)), c(rep(0, length(xx)), rev(yy)),
+        density = 20, angle = 45)
+
+abline(v = z_crit, lty = 2)
+
+# Labels
+# text(z_crit, dnorm(z_crit), expression(z[alpha]), pos = 4)
+text(z_crit, 0, expression(z[alpha]), pos = 2)
+text(2.8, 0.05, expression(alpha))
+text(0, 0.3, expression(1 - alpha))
+dev.off()
+
+# left tail rejection region
+alpha <- 0.05
+z_crit <- qnorm(alpha)
+
+pdf("left-tailed-rejection-region.pdf")
+plot(x, y, type = "l", lwd = 2,
+     main = "Rejection Region (Right-tailed Test)",
+     xlab = "Test Statistic (Z)", ylab = "Density")
+
+# Shade rejection region
+xx <- seq(z_crit, -4, length = 500)
+yy <- dnorm(xx)
+polygon(c(xx, rev(xx)), c(rep(0, length(xx)), rev(yy)),
+        density = 20, angle = 45)
+
+abline(v = z_crit, lty = 2)
+
+# Labels
+# text(z_crit, dnorm(z_crit), expression(z[alpha]), pos = 4)
+text(z_crit, 0, expression(z[alpha]), pos = 4)
+text(-2.8, 0.05, expression(alpha))
+text(0, 0.3, expression(1 - alpha))
+dev.off()
+
+# --- observed
+z_cal <- 1.34
+alpha <- 0.05
+z_crit <- qnorm(1 - alpha)
+
+pdf("observed-fail-to-reject.pdf")
+plot(x, y, type = "l", lwd = 2,
+     main = "Decision Rule with Observed Test Statistic",
+     xlab = "Test Statistic (Z)", ylab = "Density")
+
+# Rejection region
+xx <- seq(z_crit, 4, length = 500)
+yy <- dnorm(xx)
+polygon(c(xx, rev(xx)), c(rep(0, length(xx)), rev(yy)),
+        density = 20, angle = 45)
+
+# Lines
+abline(v = z_crit, lty = 2)
+abline(v = z_cal, col = 2, lwd = 2)
+
+# Labels
+text(z_crit, dnorm(z_crit), expression(z[alpha]), pos = 4)
+text(z_cal, dnorm(z_cal), expression(z[cal]), pos = 2, col = "red")
+dev.off()
+
+
+# -- observed, reject
+z_cal <- 2
+alpha <- 0.05
+z_crit <- qnorm(1 - alpha)
+
+pdf("observed-reject.pdf")
+plot(x, y, type = "l", lwd = 2,
+     main = "Decision Rule with Observed Test Statistic",
+     xlab = "Test Statistic (Z)", ylab = "Density")
+
+# Rejection region
+xx <- seq(z_crit, 4, length = 500)
+yy <- dnorm(xx)
+polygon(c(xx, rev(xx)), c(rep(0, length(xx)), rev(yy)),
+        density = 20, angle = 45)
+
+# Lines
+abline(v = z_crit, lty = 2)
+abline(v = z_cal, col = 2, lwd = 2)
+
+# Labels
+text(z_crit, dnorm(z_crit), expression(z[alpha]), pos = 2)
+text(z_cal, dnorm(z_cal), expression(z[cal]), pos = 4, col = "red")
+dev.off()
