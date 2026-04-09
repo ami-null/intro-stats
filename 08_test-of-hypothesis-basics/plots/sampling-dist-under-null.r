c11565541908ee78cf2ad0738db9d8fa415a0acb
ami-null <70378126+ami-null@users.noreply.github.com>
2026-04-10 02:13:52 +0600

    refined the plots for the slide

--- 08_test-of-hypothesis-basics/plots/sampling-dist-under-null.r
+++ 08_test-of-hypothesis-basics/plots/sampling-dist-under-null.r
@@ -4,13 +4,17 @@ y <- dnorm(x)
 
 # Plot
 
-pdf("sampling-dist-under-null.pdf", width = 6.5, height = 3.5)
+pdf("sampling-dist-under-null.pdf", height = 3.5, width = 3.5)
+par(
+    mar  = c(3.4, 3.4, 1.6, 1),  # minimal bottom/left for labels, almost nothing top/right
+    mgp  = c(2.25, 0.85, 0)
+)
 plot(x, y, type = "l", lwd = 2,
      main = expression("Sampling Distribution under " * H[0]),
      xlab = "Test Statistic (Z)", ylab = "Density")
 
 abline(v = 0, lty = 2)
-text(-0.1, max(y)*0.85, expression(mu[0]), pos = 4)
+# text(-0.1, max(y)*0.85, expression(mu[0]), pos = 4)
 dev.off()
 
 
@@ -19,19 +23,23 @@ dev.off()
 # Define an arbitrary "unlikely" cutoff (before formal alpha)
 cutoff <- 2
 
-pdf("unlikely-region-under-null.pdf")
+pdf("unlikely-region-under-null.pdf", height = 3.5, width = 3.5)
+par(
+    mar  = c(3.4, 3.4, 1.6, 1),  # minimal bottom/left for labels, almost nothing top/right
+    mgp  = c(2.25, 0.85, 0)
+)
 plot(x, y, type = "l", lwd = 2,
-     main = "Unlikely Values under H0",
+     main = expression("Unlikely Values under"~H[0]),
      xlab = "Test Statistic (Z)", ylab = "Density")
 
 # Shade right tail
-xx1 <- seq(-4, -z_crit, length = 500)
+xx1 <- seq(-4, -cutoff, length = 500)
 yy1 <- dnorm(xx1)
 polygon(c(xx1, rev(xx1)), c(rep(0, length(xx1)), rev(yy1)),
         density = 20, angle = 135)
 
 # Right tail
-xx2 <- seq(z_crit, 4, length = 500)
+xx2 <- seq(cutoff, 4, length = 500)
 yy2 <- dnorm(xx2)
 polygon(c(xx2, rev(xx2)), c(rep(0, length(xx2)), rev(yy2)),
         density = 20, angle = 45)
@@ -42,17 +50,21 @@ text(cutoff, dnorm(cutoff), "Unlikely", pos = 4)
 abline(v = -cutoff, lty = 2)
 text(-cutoff, dnorm(-cutoff), "Unlikely", pos = 2)
 
-text(0, max(y)*0.75, expression(H[0]~"True"))
+# text(0, max(y)*0.75, expression(H[0]~"True"))
 dev.off()
 
 
 # ---- alpha
-pdf("two-tail-rejection-region.pdf")
+pdf("two-tail-rejection-region.pdf", height = 3.5, width = 3.5)
+par(
+    mar  = c(3.4, 3.4, 1.6, 1),  # minimal bottom/left for labels, almost nothing top/right
+    mgp  = c(2.25, 0.85, 0)
+)
 alpha <- 0.05
 z_crit <- qnorm(1 - alpha/2)
 
 plot(x, y, type = "l", lwd = 2,
-     main = "Rejection Regions (Two-tailed Test)",
+     main = expression("Rejection Regions (Two-tailed)"),
      xlab = "Test Statistic (Z)", ylab = "Density")
 
 # Left tail
@@ -87,9 +99,13 @@ dev.off()
 alpha <- 0.05
 z_crit <- qnorm(1 - alpha)
 
-pdf("right-tailed-rejection-region.pdf")
+pdf("right-tailed-rejection-region.pdf", height = 3.5, width = 3.5)
+par(
+    mar  = c(3.4, 3.4, 1.6, 1),  # minimal bottom/left for labels, almost nothing top/right
+    mgp  = c(2.25, 0.85, 0)
+)
 plot(x, y, type = "l", lwd = 2,
-     main = "Rejection Region (Right-tailed Test)",
+     main = expression("Rejection Region (Right-tailed)"),
      xlab = "Test Statistic (Z)", ylab = "Density")
 
 # Shade rejection region
@@ -111,9 +127,13 @@ dev.off()
 alpha <- 0.05
 z_crit <- qnorm(alpha)
 
-pdf("left-tailed-rejection-region.pdf")
+pdf("left-tailed-rejection-region.pdf", height = 3.5, width = 3.5)
+par(
+    mar  = c(3.4, 3.4, 1.6, 1),  # minimal bottom/left for labels, almost nothing top/right
+    mgp  = c(2.25, 0.85, 0)
+)
 plot(x, y, type = "l", lwd = 2,
-     main = "Rejection Region (Right-tailed Test)",
+     main = expression("Rejection Region (Left-tailed)"),
      xlab = "Test Statistic (Z)", ylab = "Density")
 
 # Shade rejection region
@@ -132,13 +152,17 @@ text(0, 0.3, expression(1 - alpha))
 dev.off()
 
 # --- observed
-z_cal <- 1.34
-alpha <- 0.05
+z_cal <- 0.85
+alpha <- 0.1
 z_crit <- qnorm(1 - alpha)
 
-pdf("observed-fail-to-reject.pdf")
+pdf("observed-fail-to-reject.pdf", height = 3.5, width = 3.5)
+par(
+    mar  = c(3.4, 3.4, 1.6, 1),  # minimal bottom/left for labels, almost nothing top/right
+    mgp  = c(2.25, 0.85, 0)
+)
 plot(x, y, type = "l", lwd = 2,
-     main = "Decision Rule with Observed Test Statistic",
+     main = expression("Fail to reject"~H[0]),
      xlab = "Test Statistic (Z)", ylab = "Density")
 
 # Rejection region
@@ -158,13 +182,17 @@ dev.off()
 
 
 # -- observed, reject
-z_cal <- 2
-alpha <- 0.05
+z_cal <- 1.6
+alpha <- 0.1
 z_crit <- qnorm(1 - alpha)
 
-pdf("observed-reject.pdf")
+pdf("observed-reject.pdf", height = 3.5, width = 3.5)
+par(
+    mar  = c(3.4, 3.4, 1.6, 1),  # minimal bottom/left for labels, almost nothing top/right
+    mgp  = c(2.25, 0.85, 0)
+)
 plot(x, y, type = "l", lwd = 2,
-     main = "Decision Rule with Observed Test Statistic",
+     main = expression("Reject"~H[0]),
      xlab = "Test Statistic (Z)", ylab = "Density")
 
 # Rejection region
